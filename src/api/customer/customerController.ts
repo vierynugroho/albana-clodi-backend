4d953895bcfdb7e1ce589efb1cbac2dc9ea3116d
ranggakrisnaa <infinity.duos@gmail.com>
2025-05-05 20:44:22 +0700

    feat: init customer

---
+++ src/api/customer/customerController.ts
@@ -0,0 +1,13 @@
+import type { RequestHandler } from "express";
+
+export class CustomerController {
+	public getAllCustomers: RequestHandler = async () => {};
+
+	public getDetailCustomer: RequestHandler = async () => {};
+
+	public createCustomer: RequestHandler = async () => {};
+
+	public updateCustomer: RequestHandler = async () => {};
+
+	public deleteCustomer: RequestHandler = async () => {};
+}
---
+++ src/api/customer/customerService.ts
@@ -0,0 +1,11 @@
+export class CustomerService {
+	public getAllCustomers = async () => {};
+
+	public getDetailCustomer = async () => {};
+
+	public createCustomer = async () => {};
+
+	public updateCustomer = async () => {};
+
+	public deleteCustomer = async () => {};
+}
