7b32ab862f164d3516af4df82a2baa657d554209
Viery Nugroho <viery15102002@gmail.com>
2025-05-21 19:26:31 +0700

    fix: cors config

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,12 @@ app.set("trust proxy", true);
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(
+	cors({
+		origin: env.CORS_ORIGIN.split(","),
+		credentials: true,
+	}),
+);
 app.use(helmet());
 // app.use(rateLimiter);
 
