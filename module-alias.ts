e58a4fbbc5df6c9b4f52d33aece76cd65056acf7
vnonymous_ <61929120+vierynugroho@users.noreply.github.com>
2025-06-13 01:40:14 +0700

    Update module-alias.ts

--- module-alias.ts
+++ module-alias.ts
@@ -1,17 +1,16 @@
 import path from "node:path";
 import moduleAlias from "module-alias";
 
-console.log();
 // Log untuk menunjukkan ke mana alias @ mengarah
 if (process.env.NODE_ENV === "production") {
 	const aliasPath = path.join(__dirname, "dist/src");
-	console.log(`Alias @ mengarah ke: ${aliasPath}`);
+	console.log(`PROD - Alias @ mengarah ke: ${aliasPath}`);
 	moduleAlias.addAliases({
 		"@": aliasPath,
 	});
 } else {
 	const aliasPath = path.join(__dirname, "src");
-	console.log(`Alias @ mengarah ke: ${aliasPath}`);
+	console.log(`DEV - Alias @ mengarah ke: ${aliasPath}`);
 	moduleAlias.addAliases({
 		"@": aliasPath,
 	});
