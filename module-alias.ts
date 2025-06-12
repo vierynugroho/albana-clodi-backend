a3d95763f0f613223fb1ad2e28d68a2e6f1d3ee3
vnonymous_ <61929120+vierynugroho@users.noreply.github.com>
2025-06-13 01:49:58 +0700

    Update module-alias.ts

--- module-alias.ts
+++ module-alias.ts
@@ -1,12 +1,12 @@
-import path from "node:path";
+import path from "path";
 import moduleAlias from "module-alias";
 
 if (process.env.NODE_ENV === "production") {
 	moduleAlias.addAliases({
-		"@": `${path.join(__dirname)}/dist/src`,
+		"@": path.join(__dirname, "dist/src"),
 	});
 } else {
 	moduleAlias.addAliases({
-		"@": `${path.join(__dirname)}/src`,
+		"@": path.join(__dirname, "src"),
 	});
 }
